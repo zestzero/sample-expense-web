baf0d5862db386e01e423826a5e3821877770781
Krit B <gmkipladen@gmail.com>
2023-09-01 23:18:10 +0700

    test(expense): add unit test for ExpenseTable

--- src/components/ExpenseTable/ExpenseTable.test.tsx
+++ src/components/ExpenseTable/ExpenseTable.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, it, vitest } from "vitest";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { ExpenseTable } from "./ExpenseTable";
+import { Category, Expense } from "@models/Expense";
 
 describe("ExpenseTable", () => {
   const defaultProps = {
@@ -17,5 +18,28 @@ describe("ExpenseTable", () => {
     await waitFor(() => {
       expect(defaultProps.onToggleAll).toHaveBeenCalledTimes(1);
     });
+  });
+
+  it("should render table correctly", () => {
+    const items: Expense[] = [
+      {
+        id: "1",
+        name: "test",
+        category: Category.Accessory,
+        amount: 1,
+        date: new Date("2023-01-01"),
+      },
+    ];
+    const props = {
+      ...defaultProps,
+      items,
+    };
+    render(<ExpenseTable {...props} />);
+
+    expect(screen.getByTestId("select-all")).toBeInTheDocument();
+    expect(screen.getByText("Item")).toBeInTheDocument();
+    expect(screen.getByText("Category")).toBeInTheDocument();
+    expect(screen.getByText("Amount")).toBeInTheDocument();
+    expect(defaultProps.renderRow).toHaveBeenCalledTimes(1);
   });
 });
